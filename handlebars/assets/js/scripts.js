95ae3c12b594083c50e6c59be56bf95c541a846e
Francesco <francesco.scarnera3@gmail.com>
2020-04-23 18:19:05 +0200

    style fix

--- handlebars/assets/js/scripts.js
+++ handlebars/assets/js/scripts.js
@@ -1,3 +1,64 @@
 /**********************
  * handlebars template
  ***********************/
+
+$(document).ready(function () {
+
+    // reference 
+    var input = $('.msg-input');
+    var sent = $('.Footer i');
+    var chat = $('.Main');
+
+    // reference template
+    var source = $("#message-template").html();
+    var template = Handlebars.compile(source);
+
+    sent.click(function() {
+        var message = input.val().trim();
+
+        var data = {
+            text: message,
+            userClass: 'sent'
+        };
+
+        var html = template(data);
+        chat.append(html);
+
+        // scroll message
+        scrollMessage();
+
+        // clear input 
+        input.val('');
+
+        /***************
+            auto reply
+         **************/
+
+        setTimeout(function () {
+            var data = {
+                text: 'ok',
+                userClass: 'received'
+            };
+
+            var html = template(data);
+            chat.append(html);
+
+            // scroll message
+            scrollMessage();
+
+        }, 1000);
+    });
+
+
+ }); // end document ready
+
+// scroll message function
+
+function scrollMessage() {
+    var pixelScroll = $('.Main').height();
+
+    $('.Main').animate({
+        scrollTop: pixelScroll
+    }, 500);
+
+}
