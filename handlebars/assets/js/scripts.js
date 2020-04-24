b6590cb7a0971bdd53ae4e37311db6c7076f81c0
Francesco <francesco.scarnera3@gmail.com>
2020-04-24 16:43:14 +0200

    add time and previous messages

--- handlebars/assets/js/scripts.js
+++ handlebars/assets/js/scripts.js
@@ -13,11 +13,81 @@ $(document).ready(function () {
     var source = $("#message-template").html();
     var template = Handlebars.compile(source);
 
+
+    /********************************************
+        visible messages when the page is loaded
+     ********************************************/
+
+    // previous messages sent
+    
+    var chats = [
+        {
+            text: "Ciao come stai?",
+            time: '16.02'
+        },
+        {
+            text: "Bene grazie",
+            time: '16.03'
+        },
+        {
+            text: "Studio un po', tu?",
+            time: '16.05'
+        },
+    ];
+
+
+    for (var i = 0; i < chats.length; i++) {
+
+        var prevChat = chats[i];
+
+        var data1 = {
+            text: prevChat['text'],
+            time: prevChat['time'],
+            userClass: 'sent'
+        }
+
+        var output = template(data1);
+        chat.append(output);
+    }
+
+    // previous answers
+
+    var answers = [
+        {
+            text: "Bene, tu?",
+            time: '16.02'
+        },
+        {
+            text: "Che fai oggi?",
+            time: '16.04'
+        },
+        {
+            text: "lo stesso...",
+            time: '16.05'
+        }
+    ];
+
+    for (var i = 0; i < answers.length; i++) {
+
+        var prevAnswers = answers[i];
+        var data2 = {
+            text: prevAnswers['text'],
+            time: prevAnswers['time'],
+            userClass: 'received'
+        }
+        var output = template(data2);
+        chat.append(output);
+    }
+
+    
+
     sent.click(function() {
+
         var message = input.val().trim();
 
         var data = {
             text: message,
+            time: '16.02',
             userClass: 'sent'
         };
 
@@ -35,8 +105,10 @@ $(document).ready(function () {
          **************/
 
         setTimeout(function () {
+
             var data = {
                 text: 'ok',
+                time: '16.02',
                 userClass: 'received'
             };
 
@@ -55,6 +127,7 @@ $(document).ready(function () {
 // scroll message function
 
 function scrollMessage() {
+
     var pixelScroll = $('.Main').height();
 
     $('.Main').animate({
